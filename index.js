7d8116d102a1ce790ba9895322d4ce03382ead69
Olawale <olapetanwale@outlook.com>
2022-10-21 12:30:00 +0100

    Fix linter errors

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const navLinks = document.querySelector('.menu-list');
 const closeBtn = document.querySelector('.close-btn');
 
 hamburger.addEventListener('click', () => {
-  nav.classList.toggle('hidden')
-  nav.classList.toggle('menu')
-  navLinks.classList.toggle('hidden')
-  hamburger.classList.add('hidden')
-})
+  nav.classList.toggle('hidden');
+  nav.classList.toggle('menu');
+  navLinks.classList.toggle('hidden');
+  hamburger.classList.add('hidden');
+});
 
 closeBtn.addEventListener('click', () => {
-  nav.classList.toggle('hidden')
-  nav.classList.toggle('menu')
-  navLinks.classList.toggle('hidden')
-  hamburger.classList.remove('hidden')
-})
+  nav.classList.toggle('hidden');
+  nav.classList.toggle('menu');
+  navLinks.classList.toggle('hidden');
+  hamburger.classList.remove('hidden');
+});
