eb0afb0b8d4fc97b69d3eae6f267038bd2b13cd4
Olawale <olapetanwale@outlook.com>
2022-10-21 12:16:04 +0100

    Add mobile menu

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert('Js is loaded')
+const hamburger = document.querySelector('.menu-bars');
+const nav = document.querySelector('.navbar');
+const navLinks = document.querySelector('.menu-list');
+const closeBtn = document.querySelector('.close-btn');
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('hidden')
+  nav.classList.toggle('menu')
+  navLinks.classList.toggle('hidden')
+  hamburger.classList.add('hidden')
+})
+
+closeBtn.addEventListener('click', () => {
+  nav.classList.toggle('hidden')
+  nav.classList.toggle('menu')
+  navLinks.classList.toggle('hidden')
+  hamburger.classList.remove('hidden')
+})
